0fe25ce149155a8b2d6d14d06dd9f2911245ca93
J@7//Might Guy <90136014+J12334@users.noreply.github.com>
2022-12-16 23:12:28 +0530

    Add files via upload

---
+++ AssetManagementSystem_NIS1020/src/main/java/com/nissan/common/UserCustomException.java
@@ -0,0 +1,10 @@
+package com.nissan.common;
+
+@SuppressWarnings("serial")
+public class UserCustomException extends Exception {
+
+	public UserCustomException(String message) {
+		super(message);
+	}
+
+}
